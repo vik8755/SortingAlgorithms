84c629937cfc9f79f14d2a423ccc3cb6e3917e9d
vik8755 <kumar.vikrant8755@gmail.com>
2025-07-12 10:52:24 +0530

    Bubble sort optimised

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,13 +1,18 @@
 public class BubbleSort {
     public static Integer[] sort(Integer[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
+            // We need to optimise the sort if already is sorted
+            // then we need to break the inner loop.
+            boolean didSwapHappen=false;
             for (int j = 0; j < arr.length-1-i; j++) {
                  if(arr[j]>arr[j+1]){
                      int temp=arr[j];
                      arr[j]=arr[j+1];
                      arr[j+1]=temp;
+                     didSwapHappen=true;
                  }
             }
+            if (!didSwapHappen) break;
         }
         return arr;
     }
